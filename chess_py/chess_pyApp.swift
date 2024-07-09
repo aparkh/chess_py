4d38810e85027beb3a55100c65b744bde9f02cdd
Aliaksandr <alalparkh@gmail.com>
2024-07-09 18:00:58 +0300

    Initial Commit

---
+++ chess_py/chess_pyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chess_pyApp.swift
+//  chess_py
+//
+//  Created by Alex on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct chess_pyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
